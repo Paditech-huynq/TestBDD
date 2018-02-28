4a993af783b110ca58627cf23484483d76063bfa
huynq <huynq@paditech.org>
2018-02-28 10:17:21 +0700

    fix library and add splash screen

---
+++ app/src/main/java/com/pesteam/testbdd/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.pesteam.testbdd;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    public static final int SPLASH_TIME = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                SplashActivity.this.startActivity(intent);
+            }
+        },SPLASH_TIME);
+    }
+}
